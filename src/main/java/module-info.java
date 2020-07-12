e4c0a7e3cc5c26bc44f069fb99852699879b1d9d
Kate Gray <opensource@codebykate.com>
2020-07-12 14:59:30 -0500

    Initial Commit of template.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.aerobatt.pivcommander {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.aerobatt.pivcommander to javafx.fxml;
+    exports com.aerobatt.pivcommander;
+}
+*/
